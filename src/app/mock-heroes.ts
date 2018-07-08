ee50ca530a0e2521cfc1c2a7c468eb6bfa4f6965
Lukasz Schulz <lukasz.h.schulz@gmail.com>
2018-07-08 11:35:10 +0200

    added list of heroes with display

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Mr. Nice'},
+    {id: 12, name: 'Narco'},
+    {id: 13, name: 'Supermice'},
+    {id: 14, name: 'Bombasto'},
+    {id: 15, name: 'Celeritas'},
+    {id: 16, name: 'Magneta'},
+    {id: 17, name: 'RubberMan'},
+    {id: 18, name: 'Dynama'},
+    {id: 19, name: 'Dr IQ'},
+    {id: 20, name: 'Tornado'},
+];
+
+
+//  {}  :  ;  ''  ""
